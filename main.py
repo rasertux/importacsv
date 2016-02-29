846c2b81fd2bca0b97ab662f1aabe165c332dae6
rasertux <rafaelsergiocosta@gmail.com>
2016-02-29 18:02:39 -0300

    Adicionado verificação no caso da não existencia do arquivo.

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 import sys
-from importamodel import Importa
-from importadao import ImportaDao
+import os.path
 from importacontroller import ImportaController
 
 class Main:
-    if(len(sys.argv) != 3):
-        print("Sintaxe:")
-        print("    python importacsv/main.py path tabela")
-        print("    Onde path é o caminho para o arquivo")
-        print("    tabela é o nome da tabela no banco de dados.")
-        print("OBS:")
-        print("    A primeira linha da coluna do arquivo CSV")
-        print("    deve possuir os nomes dos campos da tabela.")
-    else:
-        control = ImportaController()
-        path = sys.argv[1]
-        tabela = sys.argv[2]
-        action = input("Digite 1 para INSERT, 2 para DELETE, 3 para UPDATE ou 0 para SAIR: ")
-        if(action == '0'):
-            sys.exit()
-        elif(action == '1'):
-            print("Importando, aguarde...")
-            if (control.importa_csv(path, tabela)):
-                print("Arquivo importado com sucesso!")
-            else:
-                print("Erro ao importar, verifique o arquivo.")
-        elif(action == '2'):
-            caution = input("Os dados serão removidos do BD, continuar? 'S' para Sim, 'N' para Não: ")
-            if(caution == 's' or caution == 'S'):
-                print("Removendo, aguarde...")
-                if (control.remove_csv(path, tabela)):
-                    print("Dados removidos com sucesso!")
-                else:
-                    print("Erro ao remover, verique o arquivo.")
-            else:
-                sys.exit()
-        elif(action == '3'):
-            where = input("Informe o nome do campo que será utilizado na cláusula where: ")
-            print("Atualizando, aguarde...")
-            if (control.atualiza_csv(path, tabela, where)):
-                print("Dados atualizados com sucesso!")
-            else:
-                print("Erro ao atualizar, verique o arquivo.")
+    def __init__(self):
+        self.control = ImportaController()
+
+    def main(self):
+        if(len(sys.argv) != 3):
+            print("Sintaxe:")
+            print("    python importacsv/main.py path tabela")
+            print("    Onde path é o caminho para o arquivo")
+            print("    tabela é o nome da tabela no banco de dados.")
+            print("OBS:")
+            print("    A primeira linha da coluna do arquivo CSV")
+            print("    deve possuir os nomes dos campos da tabela.")
+        elif(not os.path.isfile(sys.argv[1])):
+            sys.exit("Arquivo não encontrado!")
         else:
-            sys.exit("Opção inválida!")
+            path = sys.argv[1]
+            tabela = sys.argv[2]
+            action = input("Digite 1 para INSERT, 2 para DELETE, 3 para UPDATE ou 0 para SAIR: ")
+            if(action == '0'):
+                sys.exit()
+            elif(action == '1'):
+                print("Importando, aguarde...")
+                if (self.control.importa_csv(path, tabela)):
+                    print("Arquivo importado com sucesso!")
+                else:
+                    print("Erro ao importar, verifique o arquivo.")
+            elif(action == '2'):
+                caution = input("Os dados serão removidos do BD, continuar? 'S' para Sim, 'N' para Não: ")
+                if(caution == 's' or caution == 'S'):
+                    print("Removendo, aguarde...")
+                    if (self.control.remove_csv(path, tabela)):
+                        print("Dados removidos com sucesso!")
+                    else:
+                        print("Erro ao remover, verique o arquivo.")
+                else:
+                    sys.exit()
+            elif(action == '3'):
+                where = input("Informe o nome do campo que será utilizado na cláusula where: ")
+                print("Atualizando, aguarde...")
+                if (self.control.atualiza_csv(path, tabela, where)):
+                    print("Dados atualizados com sucesso!")
+                else:
+                    print("Erro ao atualizar, verique o arquivo.")
+            else:
+                sys.exit("Opção inválida!")
+
+if __name__ == "__main__":
+    m = Main()
+    m.main()
